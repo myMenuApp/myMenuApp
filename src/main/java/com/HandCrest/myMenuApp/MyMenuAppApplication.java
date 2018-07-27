a2c207e935cd6df384c8ef8976f29499e206f495
Retrojb <baltes.26@osu.edu>
2018-07-27 14:29:38 -0400

    Intial Commit

---
+++ src/main/java/com/HandCrest/myMenuApp/MyMenuAppApplication.java
@@ -0,0 +1,12 @@
+package com.HandCrest.myMenuApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMenuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMenuAppApplication.class, args);
+	}
+}
