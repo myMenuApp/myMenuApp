7b535856adfd90c599802cc3286cd91a015d0441
nzequestrian <engravethis@yahoo.com>
2018-07-30 11:36:17 -0400

    NateIsTestingWooHoo!

---
+++ src/main/java/com/HandCrest/myMenuApp/Manager.java
@@ -0,0 +1,53 @@
+package com.HandCrest.myMenuApp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Manager {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String userName;
+	private String password;
+	private String restaurant;
+
+	public Manager() {
+
+	}
+
+	public Manager(Long id, String userName, String password, String restaurant) {
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.restaurant = restaurant;
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getRestaurant() {
+		return restaurant;
+	}
+
+	@Override
+	public String toString() {
+		return "Manager [id=" + id + ", userName=" + userName + ", password=" + password + ", restaurant=" + restaurant
+				+ "]";
+	}
+
+	
+}
