1d10c58ef44ff28d71cb559418d81b3fe3c6f7c2
Retrojb <baltes.26@osu.edu>
2018-08-20 10:33:06 -0400

    added about us and tests updated

---
+++ src/test/java/com/HandCrest/myMenuApp/AdminControllerTest.java
@@ -0,0 +1,79 @@
+package com.HandCrest.myMenuApp;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+
+public class AdminControllerTest {
+
+	@Resource TestRestTemplate restTemplate;
+	@Resource private AdminController underTest;
+
+// We can add this test when we have a login html page
+	
+//	@Test public void ShouldFindAdminLoginPage () { 
+//		ResponseEntity<String> response = restTemplate.getForEntity("/login", String.class);
+//		HttpStatus status = response.getStatusCode();
+//		assertThat(status, is(HttpStatus.OK));
+//	}
+	
+// We can add this test when we have a admin html page
+	
+//	@Test public void ShouldFindAdminPage () { 
+//	ResponseEntity<String> response = restTemplate.getForEntity("/admin", String.class);
+//	HttpStatus status = response.getStatusCode();
+//	assertThat(status, is(HttpStatus.OK));
+//  }
+	
+	// We can add this test when we have a customer html page
+	
+//		@Test public void ShouldFindAdminPage () { 
+//		ResponseEntity<String> response = restTemplate.getForEntity("index/customer", String.class);
+//		HttpStatus status = response.getStatusCode();
+//		assertThat(status, is(HttpStatus.OK));
+//  }
+	
+	
+	@Test
+	public void shouldSetAdminLoginCookieFoo() {
+		HttpServletResponse response = mock(HttpServletResponse.class);
+		ArgumentCaptor<Cookie> cookieCaptor = ArgumentCaptor.forClass(Cookie.class);
+
+		underTest.adminLogin(response);
+
+		verify(response).addCookie(cookieCaptor.capture());
+		Cookie cookie = cookieCaptor.getValue();
+
+		assertThat(cookie.getName(), is("role"));
+	}
+
+	@Test
+	public void shouldSetAdminLogoutCookieFoo() {
+		HttpServletResponse response = mock(HttpServletResponse.class);
+		ArgumentCaptor<Cookie> cookieCaptor = ArgumentCaptor.forClass(Cookie.class);
+
+		underTest.adminLogin(response);
+
+		verify(response).addCookie(cookieCaptor.capture());
+		Cookie cookie = cookieCaptor.getValue();
+
+		assertThat(cookie.getName(), is("role"));
+	}
+
+}
