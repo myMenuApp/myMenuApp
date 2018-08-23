3ba5192b87ca744f98d3761b33d6791d1c682f6f
Retrojb <baltes.26@osu.edu>
2018-08-23 14:13:08 -0400

    database is fully operational

--- src/main/java/com/HandCrest/myMenuApp/RestaurantPopulator.java
+++ src/main/java/com/HandCrest/myMenuApp/RestaurantPopulator.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
-@Service
+// @Service
 public class RestaurantPopulator implements CommandLineRunner {
 	@Autowired
 	ItemRepository itemRepo;
